2e4df09172b89de9d150c3d12b9743466730e698
Dmitriy <aquarikon23@gmail.com>
2022-11-09 17:03:49 +0300

    Проба разных методов для того чтобы setValue заработал, но нихера не помогло

--- test/specs/authorityForm.js
+++ test/specs/authorityForm.js
@@ -3,15 +3,16 @@ const expect = require("chai").expect;
 describe("Autority Form", () => {
   it("Check the authorization form ", async () => {
     browser.url("https://dev.sphere.oelp.ru:7443/user/");
-    let voytiButton = $$("div.login-button > div")[0];
+    let voytiButton = $("div.login-button");
     await voytiButton.click();
     browser.pause(5000);
     let email = await $$("div.input-wrapper")[2];
     await email.click();
-    await email.setValue("test123");
+    email.setValue("test123");
+   await browser.pause(9000)
     let password = await $$("div.input-wrapper")[3];
-    await password.click();
-    await password.setValue("test123");
-    await browser.pause(5000);
+     password.click();
+     password.setValue("test123");
+     browser.pause(5000);
   });
 });
