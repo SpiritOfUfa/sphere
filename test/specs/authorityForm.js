bd5970e7bed496d19f556a4f44b49bfabc49dbe2
Dmitriy <aquarikon23@gmail.com>
2022-11-09 16:42:03 +0300

    перезагрузка коммитов

--- test/specs/authorityForm.js
+++ test/specs/authorityForm.js
@@ -3,15 +3,15 @@ const expect = require("chai").expect;
 describe("Autority Form", () => {
   it("Check the authorization form ", async () => {
     browser.url("https://dev.sphere.oelp.ru:7443/user/");
-    let voytiButton = $("div.login-button");
+    let voytiButton = $$("div.login-button > div")[0];
     await voytiButton.click();
     browser.pause(5000);
-    let email = $$("div.input-wrapper")[2];
+    let email = await $$("div.input-wrapper")[2];
     await email.click();
-    email.setValue("test1323123123123bfsd"); // Разобраться почему эта хуйня не работает
-    let password = $$("div.input-wrapper")[3];
-    password.click();
+    await email.setValue("test123");
+    let password = await $$("div.input-wrapper")[3];
+    await password.click();
+    await password.setValue("test123");
     await browser.pause(5000);
-    password.setValue("test1323123123123bfsd");
   });
 });
