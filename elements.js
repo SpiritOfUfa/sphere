885dbe1a7d1025beafd45fb3f3fda7d541de1e69
Dmitriy <aquarikon23@gmail.com>
2022-11-10 17:17:15 +0300

    Validate the login form with correct and incorrect passwords/emails

---
+++ elements.js
@@ -0,0 +1,9 @@
+export const Urls = {
+    admin : 'https://dev.sphere.oelp.ru:7443/admin',
+    landingUrl : 'https://dev.sphere.oelp.ru:7443/user/',
+
+}
+export const Hello = () => console.log('hello Towarishy')
+
+
+
--- elements/elements.js
+++
@@ -1,5 +0,0 @@
-export const Urls = {
-    admin : 'https://dev.sphere.oelp.ru:7443/admin',
-
-}
-export const hello = () => console.log('hello Towarishy')
