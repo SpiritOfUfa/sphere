738ae8dff0294f0ef90168ad9e029c91db99ac26
Dmitriy <aquarikon23@gmail.com>
2022-11-10 12:47:48 +0300

    Validate the Header,Placeholders and Buttons names

--- test/specs/authorityForm.js
+++ test/specs/authorityForm.js
@@ -1,18 +1,42 @@
 const expect = require("chai").expect;
 
-describe("Autority Form", () => {
-  it("Check the authorization form ", async () => {
+describe("Autorization Form", () => {
+  it("Check the header name and name of button  ", async () => {
     browser.url("https://dev.sphere.oelp.ru:7443/user/");
     let voytiButton = $("div.login-button");
     await voytiButton.click();
-    browser.pause(5000);
-    const email =  $("body > div:nth-child(11) > div > div > div.modal-body > form > div:nth-child(1) > input");
-     email.click();
-     email.setValue('test123');
-   await browser.pause(5000)
-    let password = await $("body > div:nth-child(11) > div > div > div.modal-body > form > div:nth-child(2) > input");
-     password.click();
-     password.setValue("test123");
-     await browser.pause(9000);
+    await browser.pause(1000);
+    let headerTitle = await $(
+      "body > div:nth-child(11) > div > div > div.modal-header > div.modal-header__toggle-buttons > h3"
+    ).getText();
+    let expectTitleText = "Вход";
+    expect(headerTitle).to.equal(expectTitleText);
   });
+  it('Check the name of placeholders(Почта, Пароль)', async() => {
+    let emailPlaceholder =  await $("body > div:nth-child(11) > div > div > div.modal-body > form > div:nth-child(1) > input").getAttribute('placeholder');
+    let expectEmailPlaceholder = "Почта";
+    expect(emailPlaceholder).to.equal(expectEmailPlaceholder);
+    let passwordPlaceholder = await $("body > div:nth-child(11) > div > div > div.modal-body > form > div:nth-child(2) > input").getAttribute("placeholder");
+    let expectPasswordPlaceholder = "Пароль"
+    expect(passwordPlaceholder).to.equal(expectPasswordPlaceholder);
+  });
+  it('Check the button name', async () => {
+    let buttonName = await $("body > div:nth-child(11) > div > div > div.modal-body > form > button").getText();
+    let expectButtonName = "войти";
+    expect(buttonName).to.equal(expectButtonName);
+  });
+    // it("Check the elements on form ", async () => {
+    //   browser.url("https://dev.sphere.oelp.ru:7443/user/");
+    //   let voytiButton = $("div.login-button");
+    //   await voytiButton.click();
+    //   browser.pause(5000);
+    //   const email =  $("body > div:nth-child(11) > div > div > div.modal-body > form > div:nth-child(1) > input");
+    //    email.click();
+    //    email.setValue('test123');
+    //  await browser.pause(5000)
+    //   let password = await $("body > div:nth-child(11) > div > div > div.modal-body > form > div:nth-child(2) > input");
+    //    password.click();
+    //    password.setValue("test123");
+    //    await browser.pause(9000);
+    // });
 });
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,12 +1,26 @@
 const expect = require("chai").expect;
 
 describe("Main page test suite", () => {
-  it("Test 1", async () => {
+  // it("Test 1", async () => {
+  //   browser.url("https://demoqa.com");
+  //   let element = await $$("#app > div > div > div.home-body > div").length;
+  //   let elementLength = await element.length;
+  //   return expect(elementLength).to.equal(6);
+  //   browser.pause(5000);
+  // });
+  it("Get text from footer", async () => {
     browser.url("https://demoqa.com");
-    let element =  await $$("div.category-cards > div").length;
-    // let elementLength =  await element.length
-   return expect(element).to.equal(6)
-     browser.pause(5000);
+    let footerText = await $("#app > footer > span").getText();
+    let text = "© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.";
+    expect(footerText).to.equal(text);
+  });
+  it('Validate the name of tiles', async () => {
+    const [elements,forms,alert,widgets,interactions,bookstore] = await $$("h5").map(el => el.getText());
+    expect(elements).to.equal("Elements");
+    expect(forms).to.equal("Forms");
+    expect(alert).to.equal("Alerts, Frame & Windows");
+    expect(widgets).to.equal("Widgets");
+    expect(interactions).to.equal("Interactions");
+    expect(bookstore).to.equal("Book Store Application");
   });
 });
-
